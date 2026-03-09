cb31873951d4dd30b308abaaf4cace49ed6a4398
unknown <leticiazdp12@gmail.com>
2026-03-09 17:13:54 +0100

    Implementar SMSService

---
+++ P4.1-notifications-base/NotificationManager.java
@@ -0,0 +1,27 @@
+// NotificationManager.java - Código a refactorizar
+public class NotificationManager{
+    // TODO: Separar en clases diferentes: EmailService, SMSService, PushService
+    // TODO: Aplicar patrón Strategy para los tipos de notificación
+    // TODO: Añadir sistema de logs
+   
+    public void send(String type, String message, String recipient) {
+        if (type.equals("email")) {
+            // Código para enviar email
+            System.out.println("Enviando email a " + recipient + ": " + message);
+            // Lógica compleja de email aquí...
+        } else if (type.equals("sms")) {
+            // Código para enviar SMS
+            System.out.println("Enviando SMS a " + recipient + ": " + message);
+            // Lógica compleja de SMS aquí...
+        } else if (type.equals("push")) {
+            // Código para notificación push
+            System.out.println("Enviando push a " + recipient + ": " + message);
+            // Lógica compleja de push aquí...
+        }
+    }
+   
+    // TODO: Añadir método para enviar a múltiples destinatarios
+    // TODO: Añadir sistema de reintentos
+    // TODO: Añadir validación de parámetros
+}
+
--- P4.1-notifications-base/SMSService.java
+++ P4.1-notifications-base/SMSService.java
@@ -1,3 +1,3 @@
-public class SMSService {
+public class SMSService implements NotificationService{
 
 }
