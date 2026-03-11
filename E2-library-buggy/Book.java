c95a6c202cd2fa4a549ad872d6ad3c99bc23bb0d
unknown <leticiazdp12@gmail.com>
2026-03-11 20:35:17 +0100

    fix:corregidos todos los bugs de la clase book (agragados todos los setters and getters, agregados tambien sistemas de validación para libros prestados y devueltos

--- E2-library-buggy/Book.java
+++ E2-library-buggy/Book.java
@@ -12,18 +12,54 @@ public class Book {
         this.available = true;
     }
     
-    // BUG 1: No hay getters/setters para todos los campos
-    public String getTitle() { return title; }
-    public String getAuthor() { return author; }
-    // Faltan getIsbn() y isAvailable()
+
+    //getters and setters
+    public String getTitle() {  return title;   }
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+
+    public String getAuthor() {return author;}
+     public void setAuthor(String author) {
+        this.author = author;
+    }
+
+
+    public String getIsbn() { return isbn; }
+    public void setIsbn(String isbn) {
+        this.isbn = isbn;
+    }
+
+
+
+    public boolean isAvailable() {return available;}
+    public void setAvailable(boolean available) {
+        this.available = available;
+    }
     
+    //metodos de la clase
+    //prestamos
     public void borrow() {
-        // BUG 2: No valida si ya está prestado
+       if (!available) {
+            System.out.println("El libro ya está prestado");
+            return;
+        }
         available = false;
+        System.out.println("Libro prestado correctamente.");
     }
     
+    //devoluciones de libros
     public void returnBook() {
-        // BUG 3: No valida si ya estaba disponible
+        if (available) {
+            System.out.println("El libro ya estaba disponible.");
+            return;
+        }
         available = true;
+        System.out.println("Libro devuelto correctamente.");
     }
-}
+
+
+    
+    }
+
